9f31ba04f0e72350a8c18dfa6c0af42e9c3487d4
Pam Selle <pamela.selle@gmail.com>
2012-10-19 21:21:48 -0400

    addressing

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var pub = __dirname + '/public';
 
 var app = express.createServer(express.logger());
+app.use(express.directory(pub))
 app.use(express.static(pub));
 
 app.set('view engine', 'jade');
