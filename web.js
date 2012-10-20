a9f8121669d89d7f127fda0adf7c208bd1874f93
Pam Selle <pamela.selle@gmail.com>
2012-10-19 20:56:07 -0400

    Pathfix

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(req, res) {
-      res.render('index', { title: 'The index page!' })
+      res.render('views/index', { title: 'The index page!' })
 });
 
 var port = process.env.PORT || 5000;
