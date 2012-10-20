ab7cd1193ea6a402f26588de87568eaf8f927556
Pam Selle <pamela.selle@gmail.com>
2012-10-20 14:22:19 -0400

    Added lots of basic styles, some lorem ipsum, and basic CSS

--- web.js
+++ web.js
@@ -5,11 +5,11 @@ var pub = __dirname + '/public';
 var app = express.createServer(express.logger());
 //app.use(express.directory(pub))
 //app.use(express.static(pub));
-
+app.use("/public", express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-      res.render('index.jade', { title: 'The index page!' })
+      res.render('index.jade', { title: 'Neighboards' })
 });
 
 var port = process.env.PORT || 5000;
