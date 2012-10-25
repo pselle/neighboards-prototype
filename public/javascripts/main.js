7e8852f4f9edccc8dc99f12f686f1518a8ca2246
Pam Selle <pamela.selle@gmail.com>
2012-10-25 18:21:41 -0400

    Edits to the delete modals, adding JS bindings

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,15 @@
 $('.login').click(function() {
-  Modal.show('#login-modal')
+  Modal.show('#login-modal');
 });
 
 $('.signup').click(function() {
-  Modal.show('#signup-modal')
+  Modal.show('#signup-modal');
+});
+
+$('.block .delete').click(function() {
+  Modal.show('#delete-modal');
+});
+
+$('.block-title').click(function() {
+  Modal.show('#block-modal');
 });
