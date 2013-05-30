23d68dea87efae4e391b12247029b2334df662bb
pselle <pamela.selle@gmail.com>
2013-05-29 22:43:29 -0700

    Fix some semicolons

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.use("/public", express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-  res.render('index.jade', { title: 'Neighboards', loggedin: globallogin })
+  res.render('index.jade', { title: 'Neighboards', loggedin: globallogin });
 });
 
 app.post('/login', function(req, res) {
@@ -26,20 +26,20 @@ app.get('/logout', function (req, res) {
 });
 
 app.get('/add-flyer', function(req, res) {
-  res.render('add.jade', { title: 'Neighboards' })
+  res.render('add.jade', { title: 'Neighboards' });
 });
 
 app.get('/success', function(req, res) {
-  res.render('thanks.jade', { title: 'Neighboards' })
+  res.render('thanks.jade', { title: 'Neighboards' });
 });
 
 app.get('/preferences', function(req, res) {
-      res.render('preferences.jade', { title: 'Neighboards' })
+      res.render('preferences.jade', { title: 'Neighboards' });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-      console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
 
-var globallogin = false
+var globallogin = false;
